c6fa7bd17877f1899c298f9f1a2166e5894bfc00
Maksymilian Kałużny <maksymilian.a.kaluzny@gmail.com>
2024-02-19 00:16:07 +0100

    deletted submodulees

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { slide as Menu} from 'react-burger-menu'
+
+function NavBar () {
+    
+    return (
+        <div>
+            <Menu>
+                <div>siemascxcxcaca</div>
+            </Menu>
+        </div>
+    )
+}
+
+export default NavBar
